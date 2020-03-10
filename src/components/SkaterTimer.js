14d0aa685a9451f0b25c9d8a4cc0cf066843432a
Jo Hooton <40861027+jo-hooton@users.noreply.github.com>
2020-03-10 20:36:24 +0000

    move counter logic up to skater time container

--- src/components/SkaterTimer.js
+++ src/components/SkaterTimer.js
@@ -1,66 +1,87 @@
 import React, { useState, useEffect } from "react";
 import useCounter from "../hooks/useCounter";
+import useInterval from "../hooks/useInterval.js";
 
 const SkaterTimer = props => {
-  const { count, setCount, up, down, reset, startCount } = useCounter(30);
-  const [active, setActive] = useState(false);
-  const [extraSeconds, setExtraSeconds] = useState(false);
-  const toggleActive = () => {
-    setActive(!active);
-  };
+  // const { count, setCount, up, down, reset, startCount } = useCounter(30);
 
-  const resetTimer = () => {
-    setActive(false);
-    reset();
-  };
+  // const [extraSeconds, setExtraSeconds] = useState(false);
 
-  const alterPenaltyTime = () => {
-    extraSeconds ? down(30) : up(30);
-    setExtraSeconds(!extraSeconds);
-  };
+  // const alterPenaltyTime = () => {
+  //   extraSeconds ? down(30) : up(30);
+  //   setExtraSeconds(!extraSeconds);
+  // };
 
-  useEffect(() => {
-    let interval = null;
-    if (active) {
-      interval = setInterval(() => {
-        down();
-      }, 1000);
-    } else if (!active) {
-      clearInterval(interval);
-    }
-    if (count <= 0 && active) {
-      reset();
-      setActive(false);
-    }
-    return () => clearInterval(interval);
-  }, [active, count]);
+  // useEffect(() => {
+  //   let interval = null;
+  //   if (active) {
+  //     props.jammer && props.setCurrentJammer(props.jammer);
+  //     interval = setInterval(() => {
+  //       down();
+  //     }, 1000);
+  //   } else if (!active) {
+  //     clearInterval(interval);
+  //   }
+  //   if (count <= 0 && active) {
+  //     reset();
+  //     setActive(false);
+  //   }
+  //   return () => clearInterval(interval);
+  // }, [active, count]);
 
-  const switchToOtherJammer = () => {
-    props.switchJammers(count, extraSeconds, props.jammer);
-  };
+  // const switchToOtherJammer = () => {
+  //   extraSeconds
+  //     ? props.setJammerSwitchTime(count - 30)
+  //     : props.setJammerSwitchTime(count);
+  //   props.switchJammers();
+  // };
 
-  useEffect(() => {
-    if (props.jammer && props.jammerSwitch) {
-      if (active) {
-        switchToOtherJammer();
-        resetTimer();
-      } else {
-        setCount(props.jammerSwitchTime);
-        toggleActive();
-      }
-      props.setJammerSwitch(false);
-    }
-  }, [props.jammerSwitch]);
+  // useEffect(() => {
+  //   if (props.jammer && props.currentJammer === props.jammer) {
+  //     if (active) {
+  //       switchToOtherJammer();
+  //       resetTimer();
+  //     } else {
+  //       setActive(true);
+  //     }
+  //   }
+  // }, [props.currentJammer]);
 
+  // useEffect(() => {
+  //   if (props.currentJammer === props.jammer) {
+  //     resetTimer();
+  //   } else {
+  //   }
+  // }, [props.currentJammer]);
+  const {
+    key,
+    active,
+    toggleActive,
+    resetTimer,
+    addPenalty,
+    subtractPenalty,
+    time,
+    teamId,
+    changeTime
+  } = props;
+
+  useInterval(() => {
+    if (active) changeTime(key);
+  }, 1000);
+
+  // const { count, setCount, up, down, reset, startCount } = useCounter(30);
   return (
     <>
-      <p>{props.jammer ? `Jammer` : `Blocker`}</p>
-      <time>{count}</time>
-      <button onClick={toggleActive}>{active ? `Pause` : `Start`}</button>
-      <button onClick={alterPenaltyTime}>
-        {extraSeconds ? `- 30` : `+ 30`}
-      </button>
-      <button onClick={resetTimer}>Reset</button>
+      <div>
+        <p>
+          {props.type} - Team {teamId}
+        </p>
+        <time>{time}</time>
+        <button onClick={toggleActive}>{active ? `Pause` : `Start`}</button>
+        <button onClick={subtractPenalty}>- 30</button>
+        <button onClick={addPenalty}>+ 30</button>
+        <button onClick={resetTimer}>Reset</button>
+      </div>
     </>
   );
 };
--- src/components/SkaterTimerContainer.js
+++ src/components/SkaterTimerContainer.js
@@ -1,46 +1,183 @@
 import React from "react";
 import { useState } from "react";
+import useInterval from "../hooks/useInterval.js";
 import SkaterTimer from "./SkaterTimer";
 import Jammers from "./Jammers";
 
 const SkaterTimerContainer = () => {
-  const [jammerSwitch, setJammerSwitch] = useState(false);
+  const [jammerSwitch, setJammerSwitch] = useState(true);
+  const [currentJammer, setCurrentJammer] = useState(null);
   const [jammerSwitchTime, setJammerSwitchTime] = useState(0);
 
-  const jammerSwitching = () => {
-    setJammerSwitch(!jammerSwitch);
+  const [skaters, setSkaters] = useState([
+    {
+      id: 3,
+      teamId: 1,
+      type: "BLOCKER",
+      time: 30,
+      active: false
+    },
+    {
+      id: 4,
+      teamId: 1,
+      type: "BLOCKER",
+      time: 30,
+      active: false
+    },
+    {
+      id: 1,
+      teamId: 1,
+      type: "JAMMER",
+      time: 30,
+      active: false
+    },
+    {
+      id: 2,
+      teamId: 2,
+      type: "JAMMER",
+      time: 30,
+      active: false
+    },
+    {
+      id: 5,
+      teamId: 2,
+      type: "BLOCKER",
+      time: 30,
+      active: false
+    },
+    {
+      id: 6,
+      teamId: 2,
+      type: "BLOCKER",
+      time: 30,
+      active: false
+    }
+  ]);
+
+  const switchJammers = () => {
+    currentJammer == 1 ? setCurrentJammer(2) : setCurrentJammer(1);
   };
 
-  const switchJammers = (count, extraSeconds, jammer) => {
-    if (!extraSeconds) {
-      setJammerSwitchTime(count);
-    } else {
-      setJammerSwitchTime(count - 30);
+  const toggleSkaterActive = skaterId => {
+    let otherActiveJammer;
+    let penaltyTime = 30;
+
+    let newSkaters = skaters.map(s => {
+      if (s.id !== skaterId) return s;
+
+      if (s.type === "JAMMER") {
+        otherActiveJammer = skaters.find(
+          s => s.type === "JAMMER" && s.active && s.id !== skaterId
+        );
+        console.log(otherActiveJammer);
+        // penaltyTime = otherActiveJammer.time;
+      }
+
+      return {
+        ...s,
+        active: !s.active,
+        time: penaltyTime
+      };
+    });
+
+    if (otherActiveJammer) {
+      newSkaters = newSkaters.map(s => {
+        if (s.id === skaterId)
+          return {
+            ...s,
+            time: 30 - otherActiveJammer.time
+          };
+        if (s.id !== otherActiveJammer.id) return s;
+
+        return {
+          ...s,
+          active: !s.active,
+          time: 30
+        };
+      });
     }
-    setJammerSwitch(true);
+
+    setSkaters(newSkaters);
   };
+
+  const changeSkaterTime = skaterId => {
+    setSkaters(
+      skaters.map(s => {
+        if (s.id !== skaterId) return s;
+
+        return {
+          ...s,
+          time: s.time - 1
+        };
+      })
+    );
+  };
+
+  const toggleSkaterTimer = skaterId => {
+    const skater = skaters.find(s => s.id === skaterId);
+    let interval = null;
+
+    if (skater.active) {
+      interval = setInterval(() => {
+        console.log(skater.time - 1);
+        changeSkaterTime(skater.time - 1);
+      }, 1000);
+    } else if (skater.time <= 0 && skater.active) {
+      resetSkaterTimer(skaterId);
+    } else {
+      clearInterval(interval);
+      resetSkaterTimer(skaterId);
+    }
+  };
+
+  //   return () => clearInterval(interval);
+
+  const resetSkaterTimer = skaterId => {
+    setSkaters(
+      skaters.map(s => {
+        if (s.id !== skaterId) return s;
+
+        return {
+          ...s,
+          active: false,
+          time: 30
+        };
+      })
+    );
+  };
+
+  const alterSkaterPenaltyTime = (skaterId, increment) => {
+    setSkaters(
+      skaters.map(s => {
+        if (s.id !== skaterId) return s;
+
+        return {
+          ...s,
+          time: s.time + increment
+        };
+      })
+    );
+  };
+
+  const addSkaterPenalty = skaterId => alterSkaterPenaltyTime(skaterId, 30);
+  const subtractSkaterPenalty = skaterId =>
+    alterSkaterPenaltyTime(skaterId, -30);
+
   return (
     <>
-      <h1>Skater Timer Container</h1>
-      <SkaterTimer jammer={false} />
-      <SkaterTimer jammer={false} />
-      <SkaterTimer
-        jammer={1}
-        jammerSwitchTime={jammerSwitchTime}
-        jammerSwitch={jammerSwitch}
-        setJammerSwitch={setJammerSwitch}
-        switchJammers={switchJammers}
-      />
-      <button onClick={jammerSwitching}>Jammer Switch</button>
-      <SkaterTimer
-        jammer={2}
-        jammerSwitchTime={jammerSwitchTime}
-        jammerSwitch={jammerSwitch}
-        setJammerSwitch={setJammerSwitch}
-        switchJammers={switchJammers}
-      />
-      <SkaterTimer jammer={false} />
-      <SkaterTimer jammer={false} />
+      <h1>Penalty Box Manager</h1>
+      <button onClick={switchJammers}>Jammer Switch</button>
+      {skaters.map(skater => (
+        <SkaterTimer
+          key={skater.id}
+          {...skater}
+          toggleActive={() => toggleSkaterActive(skater.id)}
+          changeTime={() => changeSkaterTime(skater.id)}
+          resetTimer={() => resetSkaterTimer(skater.id)}
+          addPenalty={() => addSkaterPenalty(skater.id)}
+          subtractPenalty={() => subtractSkaterPenalty(skater.id)}
+        />
+      ))}
     </>
   );
 };
